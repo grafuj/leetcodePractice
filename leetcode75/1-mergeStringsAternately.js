78aa46b437359eb9f667251f4e37ada771525e7d
Graham Fujisawa <sfrrki@gmail.com>
2023-07-21 09:27:34 -0700

    updating solution with alternating in one basic case

--- leetcode75/1-mergeStringsAternately.js
+++ leetcode75/1-mergeStringsAternately.js
@@ -7,9 +7,10 @@
 var mergeAlternately = function(word1, word2) {
   let merged = "";
   if ( word1.length > word2.length){
-      for (char in word2) {
+      for (let i = 0; i < word2.length; i++) {
           //needs to alternate
-          merged.push(char);
+          merged.push(word1[i]);
+          merged.push(word2[i]);
       }
       merged.push(word1.slice(word2.length - 1));
       return merged;
