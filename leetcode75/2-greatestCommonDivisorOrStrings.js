0269da68a619d3f1add5415e8325e35fec405da1
Graham Fujisawa <sfrrki@gmail.com>
2023-07-21 17:08:30 -0700

    adding valid function

--- leetcode75/2-greatestCommonDivisorOrStrings.js
+++ leetcode75/2-greatestCommonDivisorOrStrings.js
@@ -4,5 +4,31 @@
  * @return {string}
  */
 var gcdOfStrings = function(str1, str2) {
-    
+  if (str1 + str2 !== str2 + str1) {
+      //no common element
+      return "";
+  }
+
+  let isValid = (word, str) => {
+      
+  }
+
+  let result = "";
+
+  for (let i = 0; i < str1.length){
+      let slice = str1.slice(0, i);
+      if(isValid(slice, str2)){
+
+      }
+  }
+
+  let smallest = "";
+  if (str1 >= str2){
+      smallest = str2;
+  } else if (str2 > str1) {
+      smallest = str1;
+  } else {
+      console.log("error in lengths")
+  }
+  
 };
