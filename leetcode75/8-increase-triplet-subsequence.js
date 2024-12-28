b561a057d5348a2fcff43c3bb7ccc221e58e5ceb
grafuj <sfrrki@gmail.com>
2024-12-27 18:01:49 -0800

    integrate second for loop to only check uppers when lowers was found to be good

--- leetcode75/8-increase-triplet-subsequence.js
+++ leetcode75/8-increase-triplet-subsequence.js
@@ -6,33 +6,53 @@ function increasingTriplet(nums) {
         var upperElementsCount = nums.length - numsIndex - 1;
         var lowerElemsArray = nums.slice(0, numsIndex);
         var upperElemsArray = nums.slice(numsIndex + 1, nums.length);
+        if (smaller && greater) {
+            break;
+        }
         // break exits the for loop, continue ends the current execution/iteration
-        console.log("comparison: ", lowerElementsCount, lowerElemsArray, "index: ", numsIndex, "elem: ", nums[numsIndex], "upper: ", upperElementsCount, upperElemsArray);
+        console.log("comparison: ", "lowerArray: ", lowerElemsArray, "index: ", numsIndex, "elem: ", nums[numsIndex], "upperArray: ", upperElemsArray);
         // get lower elements array, iterate through
-        if (lowerElemsArray.length > 0) {
+        console.log("low check: ", lowerElemsArray.length > 0);
+        if (lowerElemsArray.length > 0 && upperElemsArray.length > 0) {
             for (var lowerElemsIndex = 0; lowerElemsIndex < lowerElementsCount; lowerElemsIndex++) {
-                if (smaller) {
-                    break;
-                }
                 console.log("lower elem comparison: ", lowerElemsArray[lowerElemsIndex], " < ", nums[numsIndex], lowerElemsArray[lowerElemsIndex] < nums[numsIndex]);
                 if (lowerElemsArray[lowerElemsIndex] < nums[numsIndex]) {
-                    smaller = true;
+                    for (var upperElemsIndex = 0; upperElemsIndex < upperElementsCount; upperElemsIndex++) {
+                        if (greater) {
+                            break;
+                        }
+                        console.log("upper elem comparison: ", upperElemsArray[upperElemsIndex], " > ", nums[numsIndex], upperElemsArray[upperElemsIndex] > nums[numsIndex]);
+                        if (upperElemsArray[upperElemsIndex] > nums[numsIndex]) {
+                            greater = true;
+                            smaller = true;
+                        }
+                    }
                 }
             }
         }
-        // get upper elements array, iterate through
-        console.log("up check: ", upperElemsArray.length > 0);
-        if (upperElemsArray.length > 0) {
-            for (var upperElemsIndex = 0; upperElemsIndex < upperElementsCount; upperElemsIndex++) {
-                if (greater) {
-                    break;
-                }
-                console.log("upper elem comparison: ", upperElemsArray[upperElemsIndex], " > ", nums[numsIndex], upperElemsArray[upperElemsIndex] > nums[numsIndex]);
-                if (upperElemsArray[upperElemsIndex] > nums[numsIndex]) {
-                    greater = true;
-                }
-            }
-        }
+        // // get upper elements array, iterate through
+        // console.log("up check: ", upperElemsArray.length > 0);
+        // if (upperElemsArray.length > 0) {
+        //   for (
+        //     let upperElemsIndex = 0;
+        //     upperElemsIndex < upperElementsCount;
+        //     upperElemsIndex++
+        //   ) {
+        //     if (greater) {
+        //       break;
+        //     }
+        //     console.log(
+        //       "upper elem comparison: ",
+        //       upperElemsArray[upperElemsIndex],
+        //       " > ",
+        //       nums[numsIndex],
+        //       upperElemsArray[upperElemsIndex] > nums[numsIndex]
+        //     );
+        //     if (upperElemsArray[upperElemsIndex] > nums[numsIndex]) {
+        //       greater = true;
+        //     }
+        //   }
+        // }
     }
     console.log("values smol & gre: ", smaller, greater);
     if (smaller && greater) {
@@ -42,9 +62,9 @@ function increasingTriplet(nums) {
         return false;
     }
 }
-var ex1 = [1, 2, 3, 4, 5];
-// let ex2: Array<number> = [5, 4, 3, 2, 1];
+// let ex1: Array<number> = [1, 2, 3, 4, 5];
+var ex2 = [5, 4, 3, 2, 1];
 // let ex3: Array<number> = [2, 1, 5, 0, 4, 6];
-console.log(increasingTriplet(ex1));
-// console.log(increasingTriplet(ex2));
+// console.log(increasingTriplet(ex1));
+console.log(increasingTriplet(ex2));
 // console.log(increasingTriplet(ex3));
--- leetcode75/8-increase-triplet-subsequence.ts
+++ leetcode75/8-increase-triplet-subsequence.ts
@@ -7,30 +7,31 @@ function increasingTriplet(nums: number[]): boolean {
     let lowerElemsArray: Array<number> = nums.slice(0, numsIndex);
     let upperElemsArray: Array<number> = nums.slice(numsIndex + 1, nums.length);
 
+    if (smaller && greater) {
+      break;
+    }
+
     // break exits the for loop, continue ends the current execution/iteration
 
     console.log(
       "comparison: ",
-      lowerElementsCount,
+      "lowerArray: ",
       lowerElemsArray,
       "index: ",
       numsIndex,
       "elem: ",
       nums[numsIndex],
-      "upper: ",
-      upperElementsCount,
+      "upperArray: ",
       upperElemsArray
     );
     // get lower elements array, iterate through
-    if (lowerElemsArray.length > 0) {
+    console.log("low check: ", lowerElemsArray.length > 0);
+    if (lowerElemsArray.length > 0 && upperElemsArray.length > 0) {
       for (
         let lowerElemsIndex = 0;
         lowerElemsIndex < lowerElementsCount;
         lowerElemsIndex++
       ) {
-        if (smaller) {
-          break;
-        }
         console.log(
           "lower elem comparison: ",
           lowerElemsArray[lowerElemsIndex],
@@ -39,34 +40,53 @@ function increasingTriplet(nums: number[]): boolean {
           lowerElemsArray[lowerElemsIndex] < nums[numsIndex]
         );
         if (lowerElemsArray[lowerElemsIndex] < nums[numsIndex]) {
-          smaller = true;
+          for (
+            let upperElemsIndex = 0;
+            upperElemsIndex < upperElementsCount;
+            upperElemsIndex++
+          ) {
+            if (greater) {
+              break;
+            }
+            console.log(
+              "upper elem comparison: ",
+              upperElemsArray[upperElemsIndex],
+              " > ",
+              nums[numsIndex],
+              upperElemsArray[upperElemsIndex] > nums[numsIndex]
+            );
+            if (upperElemsArray[upperElemsIndex] > nums[numsIndex]) {
+              greater = true;
+              smaller = true;
+            }
+          }
         }
       }
     }
 
-    // get upper elements array, iterate through
-    console.log("up check: ", upperElemsArray.length > 0);
-    if (upperElemsArray.length > 0) {
-      for (
-        let upperElemsIndex = 0;
-        upperElemsIndex < upperElementsCount;
-        upperElemsIndex++
-      ) {
-        if (greater) {
-          break;
-        }
-        console.log(
-          "upper elem comparison: ",
-          upperElemsArray[upperElemsIndex],
-          " > ",
-          nums[numsIndex],
-          upperElemsArray[upperElemsIndex] > nums[numsIndex]
-        );
-        if (upperElemsArray[upperElemsIndex] > nums[numsIndex]) {
-          greater = true;
-        }
-      }
-    }
+    // // get upper elements array, iterate through
+    // console.log("up check: ", upperElemsArray.length > 0);
+    // if (upperElemsArray.length > 0) {
+    //   for (
+    //     let upperElemsIndex = 0;
+    //     upperElemsIndex < upperElementsCount;
+    //     upperElemsIndex++
+    //   ) {
+    //     if (greater) {
+    //       break;
+    //     }
+    //     console.log(
+    //       "upper elem comparison: ",
+    //       upperElemsArray[upperElemsIndex],
+    //       " > ",
+    //       nums[numsIndex],
+    //       upperElemsArray[upperElemsIndex] > nums[numsIndex]
+    //     );
+    //     if (upperElemsArray[upperElemsIndex] > nums[numsIndex]) {
+    //       greater = true;
+    //     }
+    //   }
+    // }
   }
   console.log("values smol & gre: ", smaller, greater);
   if (smaller && greater) {
@@ -76,10 +96,10 @@ function increasingTriplet(nums: number[]): boolean {
   }
 }
 
-let ex1: Array<number> = [1, 2, 3, 4, 5];
-// let ex2: Array<number> = [5, 4, 3, 2, 1];
+// let ex1: Array<number> = [1, 2, 3, 4, 5];
+let ex2: Array<number> = [5, 4, 3, 2, 1];
 // let ex3: Array<number> = [2, 1, 5, 0, 4, 6];
 
-console.log(increasingTriplet(ex1));
-// console.log(increasingTriplet(ex2));
+// console.log(increasingTriplet(ex1));
+console.log(increasingTriplet(ex2));
 // console.log(increasingTriplet(ex3));
