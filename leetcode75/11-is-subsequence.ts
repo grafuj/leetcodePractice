c4175185700033d12f29fff44394bc02869b87b5
grafuj <sfrrki@gmail.com>
2025-01-01 14:16:57 -0800

    update 11 to show before and after removing elements from second string that are not in first

--- leetcode75/11-is-subsequence.ts
+++ leetcode75/11-is-subsequence.ts
@@ -1,9 +1,19 @@
 function isSubsequence(s: string, t: string): boolean {
   let splitS: Array<string> = s.split("");
-  let reducedT: string = "";
+  let splitT: Array<string> = t.split("");
+  let reducedT: Array<string> = [];
   for (let i = 0; i < t.length; i++) {
     if (splitS.indexOf(t[i]) !== -1) {
+      // not included, splice it baby
+      console.log("before reducedT: ", reducedT, "splitT: ", splitT);
+      reducedT = splitT.splice(i, 1);
+      console.log("after reducedT: ", reducedT, "splitT: ", splitT);
     }
   }
+  // console.log("reducedT: ", reducedT, "splitT: ", splitT)
   return true;
 }
+
+const ex1: string = "abc";
+const ex2: string = "abafk;sasc";
+console.log(isSubsequence(ex1, ex2));
