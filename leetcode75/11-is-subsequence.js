ec2c93d923e2938c918699d57a842be43dd34a85
carrots <sfrrki@gmail.com>
2025-01-08 14:36:08 -0800

    finish problem 11

--- leetcode75/11-is-subsequence.js
+++ leetcode75/11-is-subsequence.js
@@ -1,22 +1,17 @@
 function isSubsequence(s, t) {
-    var splitS = s.split("");
-    var splitT = t.split("");
-    var reducedT = [];
-    for (var i = 0; i < t.length; i++) {
+    if (s.length > t.length || !s.length) {
+        return false;
+    }
+    var tLength = t.length;
+    var subSequenceIndex = 0;
+    for (var i = 0; i < tLength; i++) {
         // remove  characters from t (big string) if they are not in s (small string)
-        console.log("t[i]", t[i], "splitS.indexOf(t[i]): ", splitS.indexOf(t[i]));
-        if (splitS.indexOf(t[i]) !== -1) {
-            // not included, splice it baby
-            // console.log("before reducedT: ", reducedT, "splitT: ", splitT);
-            // this line doesn't take the char as it is removed and push it onto reducedT
-            // reducedT = splitT.splice(i, 1);
-            reducedT.push(splitT[i]);
-            console.log("reducedT: ", reducedT);
-            // console.log("after reducedT: ", reducedT, "splitT: ", splitT);
+        if (s[subSequenceIndex] === t[i]) {
+            subSequenceIndex++;
         }
     }
-    // console.log("reducedT: ", reducedT, "splitT: ", splitT)
-    return true;
+    return s.length === subSequenceIndex;
+    // console.log("reducedT: ", reducedT, "splitT: ", splitT);
 }
 var ex1 = "abc";
 var ex2 = "abafk;sasc";
--- leetcode75/11-is-subsequence.ts
+++ leetcode75/11-is-subsequence.ts
@@ -1,25 +1,17 @@
 function isSubsequence(s: string, t: string): boolean {
-  if(s.length > t.length || !s.length){
+  if (s.length > t.length) {
+    // can't check for empty strings as testcases ask for "" and "" to return true >:c
     return false;
   }
-  let splitS: Array<string> = s.split("");
-  let splitT: Array<string> = t.split("");
-  let reducedT: Array<string> = [];
-  for (let i = 0; i < t.length; i++) {
-    // remove  characters from t (big string) if they are not in s (small string)
-    console.log("t[i]", t[i], "splitS.indexOf(t[i]): ", splitS.indexOf(t[i]));
-    if (splitS.indexOf(t[i]) !== -1) {
-      // not included, splice it baby
-      // console.log("before reducedT: ", reducedT, "splitT: ", splitT);
-      // this line doesn't take the char as it is removed and push it onto reducedT
-      // reducedT = splitT.splice(i, 1);
-      reducedT.push(splitT[i]);
-      console.log("reducedT: ", reducedT)
-      // console.log("after reducedT: ", reducedT, "splitT: ", splitT);
+  let tLength = t.length;
+  let subSequenceIndex: number = 0;
+  for (let i = 0; i < tLength; i++) {
+    // if the current character in small string is the same as the one in big string, we can go next in small string
+    if (s[subSequenceIndex] === t[i]) {
+      subSequenceIndex++;
     }
   }
-  // console.log("reducedT: ", reducedT, "splitT: ", splitT)
-  return true;
+  return s.length === subSequenceIndex;
 }
 
 const ex1: string = "abc";
