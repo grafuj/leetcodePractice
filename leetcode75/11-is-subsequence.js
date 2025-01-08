5368307f3036d49583fbc494162611a99994eaba
carrots <sfrrki@gmail.com>
2025-01-08 14:20:41 -0800

    add new try on subsequence with element removal when not in smaller string and pushing elements from long string to place holder when they are present in small string

--- leetcode75/11-is-subsequence.js
+++ leetcode75/11-is-subsequence.js
@@ -3,11 +3,16 @@ function isSubsequence(s, t) {
     var splitT = t.split("");
     var reducedT = [];
     for (var i = 0; i < t.length; i++) {
+        // remove  characters from t (big string) if they are not in s (small string)
+        console.log("t[i]", t[i], "splitS.indexOf(t[i]): ", splitS.indexOf(t[i]));
         if (splitS.indexOf(t[i]) !== -1) {
             // not included, splice it baby
-            console.log("before reducedT: ", reducedT, "splitT: ", splitT);
-            reducedT = splitT.splice(i, 1);
-            console.log("after reducedT: ", reducedT, "splitT: ", splitT);
+            // console.log("before reducedT: ", reducedT, "splitT: ", splitT);
+            // this line doesn't take the char as it is removed and push it onto reducedT
+            // reducedT = splitT.splice(i, 1);
+            reducedT.push(splitT[i]);
+            console.log("reducedT: ", reducedT);
+            // console.log("after reducedT: ", reducedT, "splitT: ", splitT);
         }
     }
     // console.log("reducedT: ", reducedT, "splitT: ", splitT)
--- leetcode75/11-is-subsequence.ts
+++ leetcode75/11-is-subsequence.ts
@@ -1,14 +1,21 @@
 function isSubsequence(s: string, t: string): boolean {
+  if(s.length > t.length || !s.length){
+    return false;
+  }
   let splitS: Array<string> = s.split("");
   let splitT: Array<string> = t.split("");
   let reducedT: Array<string> = [];
   for (let i = 0; i < t.length; i++) {
+    // remove  characters from t (big string) if they are not in s (small string)
+    console.log("t[i]", t[i], "splitS.indexOf(t[i]): ", splitS.indexOf(t[i]));
     if (splitS.indexOf(t[i]) !== -1) {
       // not included, splice it baby
-      console.log("before reducedT: ", reducedT, "splitT: ", splitT);
+      // console.log("before reducedT: ", reducedT, "splitT: ", splitT);
       // this line doesn't take the char as it is removed and push it onto reducedT
-      reducedT.push(splitT.splice(i, 1));
-      console.log("after reducedT: ", reducedT, "splitT: ", splitT);
+      // reducedT = splitT.splice(i, 1);
+      reducedT.push(splitT[i]);
+      console.log("reducedT: ", reducedT)
+      // console.log("after reducedT: ", reducedT, "splitT: ", splitT);
     }
   }
   // console.log("reducedT: ", reducedT, "splitT: ", splitT)
