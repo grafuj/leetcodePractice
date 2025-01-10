d751bc4a3113e75987196828187ab7ddea2359f0
carrots <sfrrki@gmail.com>
2025-01-10 10:55:28 -0800

    add working 13 with 42/51 solved console logs causing issue

--- leetcode75/13-k-sum-pairs.js
+++ leetcode75/13-k-sum-pairs.js
@@ -2,16 +2,23 @@ function maxOperations(nums, k) {
     var sortedNums = nums.sort();
     var firstIndex = 0;
     var secondIndex = nums.length - 1;
-    var halfLength = nums.length / 2;
+    var numsLength = nums.length;
     var pairsCounter = 0;
-    for (var i = 0; i < halfLength; i++) {
-        console.log("sortedNums: ", sortedNums, "firstIndex: ", firstIndex, "nums[firstIndex]: ", sortedNums[firstIndex], "+", "secondIndex: ", secondIndex, "nums[secondIndex]: ", sortedNums[secondIndex], "=", sortedNums[firstIndex] + sortedNums[secondIndex], " === ", k);
+    for (var i = 0; i < numsLength; i++) {
+        console.log("sortedNums:", sortedNums, "firstIndex:", firstIndex, "nums[firstIndex]:", sortedNums[firstIndex], "secondIndex:", secondIndex, "nums[secondIndex]:", sortedNums[secondIndex], sortedNums[firstIndex], "+", sortedNums[secondIndex], "=", sortedNums[firstIndex] + sortedNums[secondIndex], "===", k);
+        if (firstIndex === secondIndex ||
+            sortedNums.length === 1 ||
+            firstIndex > numsLength - 1 ||
+            secondIndex < 1) {
+            break;
+        }
         if (sortedNums[firstIndex] + sortedNums[secondIndex] === k) {
             console.log("equal");
             sortedNums.splice(firstIndex, 1);
             console.log("1st gone: ", sortedNums);
             secondIndex--;
             sortedNums.splice(secondIndex, 1);
+            // decrement each time an element is removed to keep it in the same spot
             secondIndex--;
             console.log("2nd gone: ", sortedNums);
             pairsCounter++;
@@ -29,5 +36,12 @@ function maxOperations(nums, k) {
     return pairsCounter;
 }
 var ex1 = [3, 1, 2, 4, 3];
-var int = 6;
-console.log(maxOperations(ex1, int));
+var ex2 = [3, 1, 3, 4, 3];
+var ex3 = [
+    4, 4, 1, 3, 1, 3, 2, 2, 5, 5, 1, 5, 2, 1, 2, 3, 5, 4,
+];
+var int6 = 6;
+var int2 = 2;
+// console.log(maxOperations(ex1, int6));
+// console.log(maxOperations(ex2, int6));
+console.log(maxOperations(ex3, int2));
--- leetcode75/13-k-sum-pairs.ts
+++ leetcode75/13-k-sum-pairs.ts
@@ -1,34 +1,47 @@
 function maxOperations(nums: number[], k: number): number {
-  let sortedNums: Array<number> = nums.sort();
+  let sortedNums: Array<number> = nums.sort((a, b) => {
+    return a - b;
+  });
   let firstIndex: number = 0;
   let secondIndex: number = nums.length - 1;
-  let halfLength: number = nums.length / 2;
+  let numsLength: number = nums.length;
   let pairsCounter: number = 0;
 
-  for (let i = 0; i < halfLength; i++) {
+  for (let i = 0; i < numsLength; i++) {
     console.log(
-      "sortedNums: ",
+      "sortedNums:",
       sortedNums,
-      "firstIndex: ",
+      "firstIndex:",
       firstIndex,
-      "nums[firstIndex]: ",
+      "nums[firstIndex]:",
+      sortedNums[firstIndex],
+      "secondIndex:",
+      secondIndex,
+      "nums[secondIndex]:",
+      sortedNums[secondIndex],
       sortedNums[firstIndex],
       "+",
-      "secondIndex: ",
-      secondIndex,
-      "nums[secondIndex]: ",
       sortedNums[secondIndex],
       "=",
       sortedNums[firstIndex] + sortedNums[secondIndex],
-      " === ",
+      "===",
       k
     );
+    if (
+      firstIndex === secondIndex ||
+      sortedNums.length === 1 ||
+      firstIndex > numsLength - 1 ||
+      secondIndex < 1
+    ) {
+      break;
+    }
     if (sortedNums[firstIndex] + sortedNums[secondIndex] === k) {
       console.log("equal");
       sortedNums.splice(firstIndex, 1);
       console.log("1st gone: ", sortedNums);
       secondIndex--;
       sortedNums.splice(secondIndex, 1);
+      // decrement each time an element is removed to keep it in the same spot
       secondIndex--;
       console.log("2nd gone: ", sortedNums);
       pairsCounter++;
@@ -44,5 +57,12 @@ function maxOperations(nums: number[], k: number): number {
   return pairsCounter;
 }
 const ex1: Array<number> = [3, 1, 2, 4, 3];
-const int: number = 6;
-console.log(maxOperations(ex1, int));
+const ex2: Array<number> = [3, 1, 3, 4, 3];
+const ex3: Array<number> = [
+  4, 4, 1, 3, 1, 3, 2, 2, 5, 5, 1, 5, 2, 1, 2, 3, 5, 4,
+];
+const int6: number = 6;
+const int2: number = 2;
+// console.log(maxOperations(ex1, int6));
+// console.log(maxOperations(ex2, int6));
+console.log(maxOperations(ex3, int2));
