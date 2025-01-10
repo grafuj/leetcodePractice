405728451aaf803e2196bb50af99188ab19338d3
carrots <sfrrki@gmail.com>
2025-01-09 20:44:44 -0800

    update 13 with working solution, still need to submit

--- leetcode75/13-k-sum-pairs.js
+++ leetcode75/13-k-sum-pairs.js
@@ -1,24 +1,33 @@
 function maxOperations(nums, k) {
     var sortedNums = nums.sort();
     var firstIndex = 0;
-    var secondIndex = nums.length;
+    var secondIndex = nums.length - 1;
     var halfLength = nums.length / 2;
     var pairsCounter = 0;
     for (var i = 0; i < halfLength; i++) {
-        console.log("sortedNums: ", sortedNums);
+        console.log("sortedNums: ", sortedNums, "firstIndex: ", firstIndex, "nums[firstIndex]: ", sortedNums[firstIndex], "+", "secondIndex: ", secondIndex, "nums[secondIndex]: ", sortedNums[secondIndex], "=", sortedNums[firstIndex] + sortedNums[secondIndex], " === ", k);
         if (sortedNums[firstIndex] + sortedNums[secondIndex] === k) {
+            console.log("equal");
             sortedNums.splice(firstIndex, 1);
+            console.log("1st gone: ", sortedNums);
+            secondIndex--;
             sortedNums.splice(secondIndex, 1);
+            secondIndex--;
+            console.log("2nd gone: ", sortedNums);
             pairsCounter++;
             i--;
         }
         else if (sortedNums[firstIndex] + sortedNums[secondIndex] > k) {
+            console.log("over");
             secondIndex--;
         }
         else if (sortedNums[firstIndex] + sortedNums[secondIndex] < k) {
+            console.log("under");
             firstIndex++;
         }
     }
     return pairsCounter;
 }
-;
+var ex1 = [3, 1, 2, 4, 3];
+var int = 6;
+console.log(maxOperations(ex1, int));
--- leetcode75/13-k-sum-pairs.ts
+++ leetcode75/13-k-sum-pairs.ts
@@ -1,25 +1,48 @@
 function maxOperations(nums: number[], k: number): number {
   let sortedNums: Array<number> = nums.sort();
   let firstIndex: number = 0;
-  let secondIndex: number = nums.length;
+  let secondIndex: number = nums.length - 1;
   let halfLength: number = nums.length / 2;
   let pairsCounter: number = 0;
 
   for (let i = 0; i < halfLength; i++) {
-    console.log("sortedNums: ", sortedNums);
+    console.log(
+      "sortedNums: ",
+      sortedNums,
+      "firstIndex: ",
+      firstIndex,
+      "nums[firstIndex]: ",
+      sortedNums[firstIndex],
+      "+",
+      "secondIndex: ",
+      secondIndex,
+      "nums[secondIndex]: ",
+      sortedNums[secondIndex],
+      "=",
+      sortedNums[firstIndex] + sortedNums[secondIndex],
+      " === ",
+      k
+    );
     if (sortedNums[firstIndex] + sortedNums[secondIndex] === k) {
+      console.log("equal");
       sortedNums.splice(firstIndex, 1);
+      console.log("1st gone: ", sortedNums);
+      secondIndex--;
       sortedNums.splice(secondIndex, 1);
+      secondIndex--;
+      console.log("2nd gone: ", sortedNums);
       pairsCounter++;
       i--;
     } else if (sortedNums[firstIndex] + sortedNums[secondIndex] > k) {
+      console.log("over");
       secondIndex--;
     } else if (sortedNums[firstIndex] + sortedNums[secondIndex] < k) {
+      console.log("under");
       firstIndex++;
     }
   }
   return pairsCounter;
 }
-const ex1: Array<number> = [3, 1, 3, 4, 3];
+const ex1: Array<number> = [3, 1, 2, 4, 3];
 const int: number = 6;
 console.log(maxOperations(ex1, int));
