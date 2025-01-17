64747a396b10bcd605eca33328c3b09f2c48e253
carrots <sfrrki@gmail.com>
2025-01-17 15:46:58 -0800

    add 21/82 solu with ignoring one zero option

--- leetcode75/17-subarray-1-delete-one.js
+++ leetcode75/17-subarray-1-delete-one.js
@@ -4,20 +4,32 @@ function longestSubarray(nums) {
     let numsLength = nums.length;
     let maxUndeleted = 0;
     let currentCount = 0;
+    let countCouldBe = 0;
+    let postFlipCount = 0;
     for (let i = 0; i < numsLength; i++) {
         if (nums[i] === 0) {
+            countCouldBe = currentCount;
             currentCount = 0;
         }
         else {
             // is 1
             currentCount++;
+            console.log("currentCount:", currentCount, "postFlipCount:", postFlipCount, "i:", i, "nums[i]:", nums[i], "countCouldBe:", countCouldBe);
+            if (nums[i + 1] === 0) {
+                postFlipCount = currentCount + countCouldBe;
+            }
             if (currentCount > maxUndeleted) {
                 maxUndeleted = currentCount;
             }
         }
     }
-    return maxUndeleted;
+    if (maxUndeleted > postFlipCount) {
+        return maxUndeleted;
+    }
+    else {
+        return postFlipCount;
+    }
 }
-;
 const ex1 = [0, 1, 1, 1, 0, 1, 1, 0, 1];
 console.log(longestSubarray(ex1));
+console.log(longestSubarray([1, 1, 0, 1]));
--- leetcode75/17-subarray-1-delete-one.ts
+++ leetcode75/17-subarray-1-delete-one.ts
@@ -1,21 +1,33 @@
 function longestSubarray(nums: number[]): number {
   let numsLength: number = nums.length;
-  
+
   let maxUndeleted: number = 0;
   let currentCount: number = 0;
-  for(let i = 0; i < numsLength; i++){
-    if(nums[i] === 0){
+  let countCouldBe: number = 0;
+  let postFlipCount: number = 0;
+  for (let i = 0; i < numsLength; i++) {
+    if (nums[i] === 0) {
+      countCouldBe = currentCount;
       currentCount = 0;
     } else {
       // is 1
       currentCount++;
-      if(currentCount > maxUndeleted){
+      console.log("currentCount:", currentCount, "postFlipCount:", postFlipCount, "i:", i, "nums[i]:", nums[i], "countCouldBe:", countCouldBe)
+      if (nums[i + 1] === 0) {
+        postFlipCount = currentCount + countCouldBe;
+      }
+      if (currentCount > maxUndeleted) {
         maxUndeleted = currentCount;
       }
     }
   }
-  return maxUndeleted;
-};
+  if (maxUndeleted > postFlipCount) {
+    return maxUndeleted;
+  } else {
+    return postFlipCount;
+  }
+}
 
-const ex1: Array<number> = [0,1,1,1,0,1,1,0,1]
-console.log(longestSubarray(ex1))
+const ex1: Array<number> = [0, 1, 1, 1, 0, 1, 1, 0, 1];
+console.log(longestSubarray(ex1));
+console.log(longestSubarray([1,1,0,1]));
