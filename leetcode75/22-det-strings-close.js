45fae0ecf5640ab1bf010f45af0678a5280a9470
carrots <sfrrki@gmail.com>
2025-02-23 16:20:58 -0800

    add working solu that can compare letter counts and ensure they're the same but doesn't compare keys

--- leetcode75/22-det-strings-close.js
+++ leetcode75/22-det-strings-close.js
@@ -4,18 +4,59 @@ function closeStrings(word1, word2) {
     if (word1.length !== word2.length) {
         return false;
     }
-    let diff1 = "";
-    let diff2 = "";
-    for (let i = 0; i < word1.length; i++) {
-        if (word1[i] !== word2[i]) {
-            diff1 += word1[i];
-            diff2 += word2[i];
+    // let diff1: string = "";
+    // let diff2: string = "";
+    // for (let i = 0; i < word1.length; i++) {
+    //   if (word1[i] !== word2[i]) {
+    //     diff1 += word1[i];
+    //     diff2 += word2[i];
+    //   }
+    // }
+    // console.log("diffs:", diff1, diff2);
+    // create object of letter counts that occur in both diffs
+    let letterObj = {};
+    for (let char of word1) {
+        // console.log("char:", char);
+        if (letterObj[char] === undefined) {
+            letterObj[char] = 1;
+        }
+        else {
+            letterObj[char]++;
         }
     }
-    console.log("diffs:", diff1, diff2);
-    // create object of letter counts that occur in both diffs
+    // console.log(letterObj)
+    let arr1 = [];
+    for (let key in letterObj) {
+        // console.log("key:", key, letterObj[key])
+        arr1.push(letterObj[key]);
+    }
+    arr1.sort();
+    // console.log(arr1);
     // sort the values of these counts and they should be the same. eg 1, 3, 5 works with 5, 1, 3
+    let letterObj2 = {};
+    for (let char of word2) {
+        // console.log("char:", char);
+        if (letterObj2[char] === undefined) {
+            letterObj2[char] = 1;
+        }
+        else {
+            letterObj2[char]++;
+        }
+    }
+    // console.log(letterObj2)
+    let arr2 = [];
+    for (let key in letterObj2) {
+        // console.log("key:", key, letterObj2[key])
+        arr2.push(letterObj2[key]);
+    }
+    arr2.sort();
+    // console.log(arr2);
     // return true if equal, false if not equal. array or string comparison?
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
     return true;
 }
 console.log(closeStrings("abbcb", "baaca"));
--- leetcode75/22-det-strings-close.ts
+++ leetcode75/22-det-strings-close.ts
@@ -3,23 +3,69 @@ function closeStrings(word1: string, word2: string): boolean {
     return false;
   }
 
-  let diff1: string = "";
-  let diff2: string = "";
-  for (let i = 0; i < word1.length; i++) {
-    if (word1[i] !== word2[i]) {
-      diff1 += word1[i];
-      diff2 += word2[i];
-    }
-  }
+  // let diff1: string = "";
+  // let diff2: string = "";
+  // for (let i = 0; i < word1.length; i++) {
+  //   if (word1[i] !== word2[i]) {
+  //     diff1 += word1[i];
+  //     diff2 += word2[i];
+  //   }
+  // }
 
-  console.log("diffs:", diff1, diff2);
+  // console.log("diffs:", diff1, diff2);
 
   // create object of letter counts that occur in both diffs
+  let letterObj: any = {};
+
+  for (let char of word1) {
+    // console.log("char:", char);
+    if (letterObj[char] === undefined) {
+      letterObj[char] = 1;
+    } else {
+      letterObj[char]++;
+    }
+  }
+  // console.log(letterObj)
+
+  let arr1: Array<number> = [];
+
+  for (let key in letterObj) {
+    // console.log("key:", key, letterObj[key])
+    arr1.push(letterObj[key])
+  }
+  arr1.sort();
+  // console.log(arr1);
+
 
   // sort the values of these counts and they should be the same. eg 1, 3, 5 works with 5, 1, 3
 
-  // return true if equal, false if not equal. array or string comparison?
+  let letterObj2: any = {};
 
+  for (let char of word2) {
+    // console.log("char:", char);
+    if (letterObj2[char] === undefined) {
+      letterObj2[char] = 1;
+    } else {
+      letterObj2[char]++;
+    }
+  }
+  // console.log(letterObj2)
+
+  let arr2: Array<number> = [];
+
+  for (let key in letterObj2) {
+    // console.log("key:", key, letterObj2[key])
+    arr2.push(letterObj2[key])
+  }
+  arr2.sort();
+  // console.log(arr2);
+
+  // return true if equal, false if not equal. array or string comparison?
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
   return true;
 }
 
