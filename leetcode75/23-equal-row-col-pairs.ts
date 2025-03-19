57565479e1b9c3c715a2dd0095785fc472330737
carrots <sfrrki@gmail.com>
2025-03-19 13:50:38 -0700

    add initial comment for how to proceed

--- leetcode75/23-equal-row-col-pairs.ts
+++ leetcode75/23-equal-row-col-pairs.ts
@@ -1,4 +1,18 @@
 function equalPairs(grid: number[][]): number {
-    
+  const columnLength: number = grid.length;
+  // rows first
+  for( let row of grid){
+    for (let elem of row){
+      if ()
+    }
+  }
+
+  // we need to check through all the column and grid combinations, but we only check after the first number if they match
+
   return 0;
 };
+
+// expects one, row 2, col 1 with 2,7,7
+console.log(equalPairs([[3,2,1],[1,7,6],[2,7,7]]))
+// expects three
+// console.log(equalPairs([[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]))
